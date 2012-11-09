1501517c0d81afe01a26fa6b43c62045387fb88a
Mathieu Tricoire <mathieu.tricoire@gmail.com>
2012-11-09 18:47:42 +0100

    Fix indentation

--- mtTimer.js
+++ mtTimer.js
@@ -23,36 +23,36 @@
  * @example Go: http://jsfiddle.net/tricky21/4FZXh/
  */
 angular.module('mtTimer', []).
-	value('version', '0.1').
-	factory('timer', ['$timeout', function($timeout) {
-		return function (ms, step_ms, callback) {
-			step_ms = angular.isNumber(step_ms) ? parseInt(step_ms, 10) : 1000;
+  value('version', '0.1').
+  factory('timer', ['$timeout', function($timeout) {
+    return function (ms, step_ms, callback) {
+      step_ms = angular.isNumber(step_ms) ? parseInt(step_ms, 10) : 1000;
 
-			var initial = parseInt(ms, 10),
-				left_ms = initial + step_ms;
+      var initial = parseInt(ms, 10),
+        left_ms = initial + step_ms;
 
-			var timer = {
-				initial: initial,
-			    left: {
-			    	ms: left_ms,
-			    	percent: 100
-				}
-			};
+      var timer = {
+        initial: initial,
+          left: {
+            ms: left_ms,
+            percent: 100
+        }
+      };
 
-	    (function tick() {
-      	left_ms = left_ms - step_ms;
+      (function tick() {
+        left_ms = left_ms - step_ms;
 
-      	if (left_ms > 0) {
-        	$timeout(tick, step_ms);
-      	} else if (typeof callback === 'function') {
-        	$timeout(callback);
-      	}
+        if (left_ms > 0) {
+          $timeout(tick, step_ms);
+        } else if (typeof callback === 'function') {
+          $timeout(callback);
+        }
 
-      	timer.initial = initial;
-      	timer.left.ms = left_ms;
-      	timer.left.percent = Math.round(left_ms / initial * 100);
-	    })();
+        timer.initial = initial;
+        timer.left.ms = left_ms;
+        timer.left.percent = Math.round(left_ms / initial * 100);
+      })();
 
-	    return timer;
-		};
-	}]);
+      return timer;
+    };
+  }]);
